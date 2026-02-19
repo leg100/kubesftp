0fe79fec541c80c83af56f2c7f2d84a7098c3865
Louis Garman <louisgarman@gmail.com>
2026-02-19 10:04:42 +0000

    main

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"log/slog"
+	"os/signal"
+	"syscall"
 
 	"github.com/leg100/kubesftp/internal"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 func main() {
-	if err := internal.Run(); err != nil {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-ctx.Done()
+		// Stop handling ^C; another ^C will exit the program.
+		cancel()
+	}()
+	if err := run(ctx); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := internal.LoadConfig()
+	if err != nil {
+		return err
+	}
+	restCfg, err := rest.InClusterConfig()
+	if err != nil {
+		return fmt.Errorf("building in-cluster config: %w", err)
+	}
+	k8s, err := kubernetes.NewForConfig(restCfg)
+	if err != nil {
+		return fmt.Errorf("creating kubernetes client: %w", err)
+	}
+	secrets := k8s.CoreV1().Secrets(cfg.Namespace)
+
+	g := &internal.Generator{
+		Logger:  slog.New(nil),
+		Secrets: secrets,
+		Config:  cfg,
+	}
+	g.Generate(ctx)
+
+	return err
 }
